848dca3ddec468c47e74ffe1161d8cf63a639e45
Kristy Thompson <105332803+Kristy-H-Thompson@users.noreply.github.com>
2023-09-01 08:05:24 -0400

    here

--- src/containers/about/About.jsx
+++ src/containers/about/About.jsx
@@ -8,8 +8,14 @@ const About = () => {
         <div className="introduction">
         <h2>Who am I?</h2>
         <hr />
-        <p>If you made it this far into my portfolio, you are probably curious about the person behind all of this code. I'm Kristy Thompson, a former teacher turned web developer. My journey into the world of coding started as a creative endeavor with a group of close online friends. We would devise an idea for a project(such as Starstar), research how to make it happen, tackle coding challenges as a team, and joyously share the projects we created with others. This collaborative spirit remains one of my favorite aspects of coding even to this day. I believe an individual's skills in coding are important, but working collaboratively with your team is just as essential to the success of a project. What began as a hobby has resulted in a genuine passion for writing code.</p>
-        <p>Outside of coding, I also enjoy spending time with my three rescue dogs, reading every book I can get my hands on, and traveling.</p>
+        <p>If you made it this far into my portfolio, you are probably curious about the person behind all of this code. <b>I am Kristy Thompson, a former teacher turned web developer.</b> My journey into the world of coding started as a creative endeavor with a group of close online friends. We would devise an idea for a project(such as Starstar), research how to make it happen, tackle coding challenges as a team, and joyously share the projects we created with others. This collaborative spirit remains one of my favorite aspects of coding even to this day. I believe an individual's skills in coding are important, but working collaboratively with your team is just as essential to the success of a project. What began as a hobby has resulted in a genuine passion for writing code and building projects.</p>
+        <p>Outside of coding I also enjoy: </p>
+        <ul>
+          <li>hiking with my three rescue dogs</li>
+          <li>reading every and any book I can get my hands on</li>
+          <li>traveling to new places with friends</li>
+          <li>and photographing my adventures</li>
+        </ul>
         </div>
         <img className="laptop" alt="laptop and books" src ={about}/>
 
