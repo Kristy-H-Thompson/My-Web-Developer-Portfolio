8b6225c0f80d674f553b32abee9ac3eac18489f7
Kristy Thompson <105332803+Kristy-H-Thompson@users.noreply.github.com>
2023-09-01 08:22:04 -0400

    lets go

--- src/containers/about/About.jsx
+++ src/containers/about/About.jsx
@@ -8,7 +8,7 @@ const About = () => {
         <div className="introduction">
         <h2>Who am I?</h2>
         <hr />
-        <p>If you made it this far into my portfolio, you are probably curious about the person behind all of this code. <b>I am Kristy Thompson, a former teacher turned web developer.</b> My journey into the world of coding started as a creative endeavor with a group of close online friends. We would devise an idea for a project(such as Starstar), research how to make it happen, tackle coding challenges as a team, and joyously share the projects we created with others. This collaborative spirit remains one of my favorite aspects of coding even to this day. I believe an individual's skills in coding are important, but working collaboratively with your team is just as essential to the success of a project. What began as a hobby has resulted in a genuine passion for writing code and building projects.</p>
+        <p>If you made it this far into my portfolio, you are probably curious about the person behind all of this code. <b>I am Kristy Thompson, a former teacher turned web developer.</b> My journey into the world of coding started as a creative endeavor with a group of close online friends. We would devise an idea for a project(such as Starstar), research how to make it happen, tackle coding challenges as a team, and joyously share the projects we created with others. This collaborative spirit remains one of my favorite aspects of coding even to this day. I believe an individual's skills in coding are important, but working collaboratively with your team is just as essential to the success of a project. What began as a hobby has resulted in a genuine passion for learning new technologies, writing code, and building projects.</p>
         <p>Outside of coding I also enjoy: </p>
         <ul>
           <li>\hiking with my three rescue dogs</li>
@@ -22,7 +22,7 @@ const About = () => {
         <div className="tech"> 
           <h2>What do I do?</h2>
           <hr />
-          <p>Little paragraph coming soon</p>
+          <p>As a front-end software engineer, I am passionate about creating immersive websites and apps that flawlessly blend form with function. My ever growing coding toolkit currently consists of HTML, CSS, JavaScript, React, Python, MySQL, Sass, and Figma.</p>
 
           <div className="tech-grid">
               <img alt="html" src={html} />
