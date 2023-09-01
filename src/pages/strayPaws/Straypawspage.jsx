c1a53fa59aeffa1016eaf52d61c18f9376117687
Kristy Thompson <105332803+Kristy-H-Thompson@users.noreply.github.com>
2023-09-01 11:47:55 -0400

    go

--- src/pages/strayPaws/Straypawspage.jsx
+++ src/pages/strayPaws/Straypawspage.jsx
@@ -8,7 +8,7 @@ const Straypawspage = () => {
   return (
     <>
     <Nav />
-    <Projectoverview projectName="Stray Paws" projectType="website commission from an animal recue" projectImage={strayPaws} projectGithub="" projectWebsite=""> 
+    <Projectoverview projectName="Stray Paws" projectType="A website commission from an animal recue" projectImage={strayPaws} projectGithub="" projectWebsite=""> 
     Stray Paws Rescue is a foster-based rescue located in Missouri. While transitioning their software solution from Rescue Groups to Shelterluv, they recognized a need for additional assistance. My team and I built them a new website that uses a Flask backend to create a flexible modular page design and API calls to load their adoptable animals in real time.
     </Projectoverview>
 
