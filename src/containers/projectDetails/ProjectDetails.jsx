040080954d2a893403669532105d059422d26778
Kristy Thompson <105332803+Kristy-H-Thompson@users.noreply.github.com>
2023-09-01 12:46:25 -0400

    yo

--- src/containers/projectDetails/ProjectDetails.jsx
+++ src/containers/projectDetails/ProjectDetails.jsx
@@ -1,17 +1,17 @@
 import React from 'react'
+import './projectOverview.css'
+
 
 const ProjectDetails = (props) => {
   return (
-    <section>
+    <section className="projectDetails">
         <h2>Project Details</h2>
         <hr />
-        <p>Reason for buiding project:{props.projectReason}</p>
-        <p></p>
-        <p>{props.children}</p>
         <ul>
             <li><b>Project Client:</b> {props.projectClient}</li>
             <li><b>Project Team:</b> {props.projectTeam}</li>
             <li><b>Project Timeline:</b> {props.projectTimeline}</li>
+            <br/>
             <li><b>Project Goals:</b> {props.children}</li>
         </ul>
     </section>
--- src/pages/strayPaws/Straypawspage.jsx
+++ src/pages/strayPaws/Straypawspage.jsx
@@ -13,7 +13,7 @@ const Straypawspage = () => {
     Stray Paws Rescue is a foster-based rescue located in Missouri. While transitioning their software solution from Rescue Groups to Shelterluv, they recognized a need for additional assistance. My team and I built them a new website that uses a Flask backend to create a flexible modular page design and API calls to load their adoptable animals in real time.
     </Projectoverview>
 
-    <Projectdetails projectClient="Stray Paws Rescue" projectTeam="Myself and Jessica Teichman" projectTimeine="One month">
+    <Projectdetails projectClient="Stray Paws Rescue" projectTeam="Myself and Jessica Teichman" projectTimeline="One month">
       <ul>
         <li>Assist the rescue in transitioning their data from Rescue Groups to Shelterluv</li>
         <li>Build an updated website UI based on an agreed upon figma design</li>
