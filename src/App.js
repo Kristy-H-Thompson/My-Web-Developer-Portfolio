93d1a389c4b1d08c9becea774f1ebc259471e5e4
Kristy Thompson <105332803+Kristy-H-Thompson@users.noreply.github.com>
2023-08-31 22:05:16 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import './App.css';
-import { About, Contact, Header, Footer, Nav, Projects} from './containers';
-import { BrowserRouter } from "react-router-dom"
+import { BrowserRouter, Routes, Route } from "react-router-dom"
 import{home, nopage, starstarpage, straypawspage} from './pages'
 
 
