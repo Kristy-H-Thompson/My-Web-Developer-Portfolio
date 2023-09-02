2cd268129dd107aa2e13869f090382116ed4a601
Kristy Thompson <105332803+Kristy-H-Thompson@users.noreply.github.com>
2023-09-02 08:48:20 -0400

    go

--- src/components/index.js
+++ src/components/index.js
@@ -2,7 +2,7 @@ export {default as Button } from './button/Button';
 export {default as ContactMe } from './contactMe/ContactMe';
 export {default as DividerBottom } from './dividerBottom/DividerBottom';
 export {default as DividerTop } from './dividerTop/DividerTop';
-export {default as intro } from './introduction/introduction';
+export {default as intro } from './introduction/Introduction';
 export {default as Scroll } from './scroll/crollToTop';
 export {default as Techgrid } from './techgrid/Techgrid';
 export {default as Toggle } from './toggle/Toggle';
