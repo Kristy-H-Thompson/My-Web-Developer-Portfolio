8d9fb733bf8bf3715a0322fc701473f86b608b4e
Kristy Thompson <kristy.h.thompson@gmail.com>
2024-11-21 20:54:49 -0700

    added in resume

--- src/containers/nav/Nav.jsx
+++ src/containers/nav/Nav.jsx
@@ -1,40 +1,67 @@
 import React from 'react'
 import './nav.css';
-import {Scroll} from '../../components';
-import {mylogo} from '../../assets'
-import {Link } from "react-router-dom";
+import { Link, useNavigate } from 'react-router-dom';
+import {mylogo, resumeDocument} from '../../assets'
 
 
 
 const Nav = () => {
+  const navigate = useNavigate();
+
+  // Function to handle navigation to the homepage and scroll to the section
+  const goToSection = (sectionId) => {
+    navigate('/');  // Navigate to the home page
+    setTimeout(() => {
+      const section = document.getElementById(sectionId);
+      if (section) {
+        section.scrollIntoView({ behavior: 'smooth' });
+      }
+    }, 100);  // Delay for page load
+  };
+
+  // Handle scrolling when clicking a link
+  const handleScroll = (e, sectionId) => {
+    e.preventDefault();  // Prevent default anchor link behavior
+    goToSection(sectionId);  // Scroll to the section after navigating
+  };
+
   return (
     <>
       <nav id="navBar">
-        <Link className="first_link" to="/" onClick={Scroll}>
+        {/* Logo */}
+        <Link className="first_link" to="/" onClick={() => goToSection('about')}>
           <img alt="My icon" className="my-icon" src={mylogo} />
         </Link>
 
-        <ul id="menu"> 
-          <li><a href="#about">About Me</a></li>      			
-          <li><a href="#projects">Portfolio</a></li>
-          <li><a href="#contact">Contact</a></li> 
-          <li><a href="#contact">Resume</a></li> 
-          
+        {/* Navigation Menu */}
+        <ul id="menu">
+          <li>
+            <a href="#about" onClick={(e) => handleScroll(e, 'about')}>About Me</a>
+          </li>              
+          <li>
+            <a href="#projects" onClick={(e) => handleScroll(e, 'projects')}>Portfolio</a>
+          </li>
+          <li>
+            <a href="#contact" onClick={(e) => handleScroll(e, 'contact')}>Contact</a>
+          </li> 
+          <li>
+            <a href={resumeDocument} download="MyExampleDoc" target='_blank'>Resume</a>
+          </li> 
         </ul>
 
+        {/* Mobile Menu Toggle */}
         <div id="menuToggle" className="handle">
           <a href="#home">
             <img alt="My icon" className="my-icon" src="https://i.imgur.com/OBpKPl1.png" />
           </a> 
         </div>
       </nav>
-      <div id="pushBack" style={{height:'0em', display:'none'}}></div>
+      <div id="pushBack" style={{ height: '0em', display: 'none' }}></div>
     </>
   );
 };
 
 export default Nav;
-
 
 
 
